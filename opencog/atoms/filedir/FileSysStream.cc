909add5b40108ad54a9c49724164c66cc2b4338a
Linas Vepštas <linasvepstas@gmail.com>
2024-04-27 19:21:39 -0500

    Add filesys scafolding

---
+++ opencog/atoms/filedir/FileSysStream.cc
@@ -0,0 +1,84 @@
+/*
+ * opencog/atoms/sensory/FileSysStream.cc
+ *
+ * Copyright (C) 2020 Linas Vepstas
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <errno.h>
+#include <string.h> // for strerror()
+
+#include <opencog/util/exceptions.h>
+#include <opencog/util/oc_assert.h>
+#include <opencog/atoms/base/Node.h>
+#include <opencog/atoms/value/StringValue.h>
+#include <opencog/atoms/value/ValueFactory.h>
+
+#include <opencog/atoms/sensory-types/sensory_types.h>
+#include "FileSysStream.h"
+
+using namespace opencog;
+
+FileSysStream::FileSysStream(const Handle& senso)
+	: OutputStream(FILE_SYS_STREAM)
+{
+	if (SENSORY_NODE != senso->get_type())
+		throw RuntimeException(TRACE_INFO,
+			"Expecting SensoryNode, got %s\n", senso->to_string().c_str());
+
+	init(senso->get_name());
+}
+
+FileSysStream::~FileSysStream()
+{
+	if (_fh)
+		fclose (_fh);
+}
+
+void FileSysStream::init(const std::string& url)
+{
+	_fh = nullptr;
+}
+
+// ==============================================================
+
+// This will read one line from the file stream, and return that line.
+// So, a line-oriented, buffered interface. For now.
+void FileSysStream::update() const
+{
+	if (nullptr == _fh) { _value.clear(); return; }
+}
+
+// ==============================================================
+// Write stuff to a file.
+
+// Write stuff to a file.
+ValuePtr FileSysStream::write_out(AtomSpace* as, bool silent,
+                                   const Handle& cref)
+{
+	if (nullptr == _fh)
+		throw RuntimeException(TRACE_INFO,
+			"Text stream not open: URI \"%s\"\n", _uri.c_str());
+
+	return createLinkValue();
+}
+
+// ==============================================================
+
+// Adds factory when library is loaded.
+DEFINE_VALUE_FACTORY(FILE_SYS_STREAM, createFileSysStream, Handle)
---
+++ opencog/atoms/filedir/FileSysStream.h
@@ -0,0 +1,70 @@
+/*
+ * opencog/atoms/sensory/FileSysStream.h
+ *
+ * Copyright (C) 2024 Linas Vepstas
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENCOG_FILE_SYS_STREAM_H
+#define _OPENCOG_FILE_SYS_STREAM_H
+
+#include <stdio.h>
+#include <opencog/atoms/sensory/OutputStream.h>
+
+namespace opencog
+{
+
+/** \addtogroup grp_atomspace
+ *  @{
+ */
+
+/**
+ * FileSysStreams provide a stream of ItemNodes read from a text file,
+ * and, more generally, from unix socket sources. This is experimental.
+ */
+class FileSysStream
+	: public OutputStream
+{
+protected:
+	void init(const std::string&);
+	virtual void update() const;
+
+	std::string _uri;
+	mutable FILE* _fh;
+	virtual void do_write(const std::string&);
+
+public:
+	FileSysStream(const Handle&);
+	virtual ~FileSysStream();
+
+	virtual ValuePtr write_out(AtomSpace*, bool, const Handle&);
+};
+
+typedef std::shared_ptr<FileSysStream> FileSysStreamPtr;
+static inline FileSysStreamPtr FileSysStreamCast(ValuePtr& a)
+	{ return std::dynamic_pointer_cast<FileSysStream>(a); }
+
+template<typename ... Type>
+static inline std::shared_ptr<FileSysStream> createFileSysStream(Type&&... args) {
+   return std::make_shared<FileSysStream>(std::forward<Type>(args)...);
+}
+
+/** @}*/
+} // namespace opencog
+
+#endif // _OPENCOG_FILE_SYS_STREAM_H
