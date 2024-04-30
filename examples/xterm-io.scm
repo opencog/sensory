2910ada77a058975ab6c9179487ef3dd32690310
Linas Vepštas <linasvepstas@gmail.com>
2024-04-29 21:49:30 -0500

    Copy the file-io demo over

--- examples/xterm-io.scm
+++ examples/xterm-io.scm
@@ -15,125 +15,66 @@
 ; Create an xterm for direct I/O. Executing the OpenLink will return
 ; a stream value that deliver text strings typed into the xterm window,
 ; and can print text to that window.
-(define txt-stream
+(define term-stream
 	(cog-execute! (Open (Type 'TerminalStream))))
 
 ; Repeated references to the stream will return single lines from
-; the file.
-txt-stream
-txt-stream
-txt-stream
-txt-stream
-txt-stream
+; the xterm window. These will hang, unless/until something is typed
+; into the terminal. The interface is line-oriented, you have to hit
+; enter at the end-of line.
+term-stream
+term-stream
+term-stream
+term-stream
 
-; Eventually, this will return an empty stream. This denotes end-of-file.
+; Typing a ctrl-D into ther terminal will close it, returning an empty
+; stream. (aka "end of file")
 
-; --------------------------------------------------------
-; Demo: Perform indirect streaming. The file-stream will be placed
-; as a Value on some Atom, where it can be accessed and processed.
-;
-; Open the file, get the stream, and place it somewhere.
-(cog-set-value! (Concept "foo") (Predicate "some place")
-	(cog-execute!
-		(Open (Type 'TextFileStream)
-			(Sensory "file:///tmp/demo.txt"))))
+; A WriteLink consists of two parts: where to write, and what
+; to write. Since the write cursor is a Value, not an Atom, we
+; cannot specify it directly. Thus, we place it at an anchor
+; point. (Neither the name "xterm anchor", nor the key "output place"
+; matter. They can be anything, and any atom can be used in their
+; place, including Links.)
+(cog-set-value!
+	(Concept "xterm anchor") (Predicate "output place") term-stream)
 
-; A better, all-Atomese version of the above. Note that the SetValueLink
-; will execute the TextFileNode, grab whatever it gets from that exec,
-; and then places it at the indicated location.
-(cog-execute!
-	(SetValue (Concept "foo") (Predicate "some place")
-		(Open (Type 'TextFileStream)
-			(Sensory "file:///tmp/demo.txt"))))
+; Create a WriteLink
+(define writer
+	(WriteLink
+		(ValueOf (Concept "xterm anchor") (Predicate "output place"))
+		(Concept "stuff to write to the terminal\n")))
 
-; Define an executable node that will feed the stream of text.
-; The ValueOf Atom is a kind of promise about the future: when
-; it is executed, it will return the Value, whatever it is, at
-; that time (at the time when the executiion is done).
-(define txt-stream-gen
-	(ValueOf (Concept "foo") (Predicate "some place")))
+; Write stuff to the terminal.
+(cog-execute! writer)
 
-; Access the file contents. Each time this is executed, it gets the
-; next line in the file.
-(cog-execute! txt-stream-gen)
-(cog-execute! txt-stream-gen)
-(cog-execute! txt-stream-gen)
-(cog-execute! txt-stream-gen)
-(cog-execute! txt-stream-gen)
+; Do it a few more times.
+(cog-execute! writer)
+(cog-execute! writer)
+(cog-execute! writer)
 
 ; --------------------------------------------------------
-; Demo: Perform processing on the stream. For each line of the
-; input file, apply a rule to rewrite it into a different form.
+; Demo: Perform indirect streaming. The text to write will be placed as
+; a StringValue at some location, and writing will be done from there.
 
-; This rule just makes two copies of each input line, interleaving
-; it with other text. It is built with the same ValueOf promise, as
-; above.
-(define rule-applier
-	; When a filter is executed, it applies the first argument
-	; to the second. The first argument here is a Rule, the
-	; second is the text file stream.
-	(Filter
-		; A rewrite rule has three parts: a variable declaration,
-		; a pattern to match, and the rewrite to apply. Here, the
-		; pattern match is trivial: `(Variable "$x")` matches
-		; everything, the entire body of the input, which will be
-		; a line from the text file. More pecisely, and ItemNode
-		; holding that line. The rewrite below is just some
-		; silliness that makes two copies of the input.
-		;
-		; The LinkSignatureLink is a constructor: it creates either
-		; a LinkValue or a Link of the specified type. In this demo,
-		; just using a plain-old List (instead of the LinkSignature)
-		; would have been OK. But if the Variable had been a Value,
-		; then using the LinkSignature would have been required.
-		(Rule
-			(TypedVariable (Variable "$x") (Type 'ItemNode))
-			(Variable "$x")
-			(LinkSignature       ; Or use List here and skip next line.
-				(Type 'LinkValue)
-				(Variable "$x")
-				(Item "yo the first\n")
-				(Variable "$x")
-				(Item "yo the second\n")
-				(Item "====\n")))
-		txt-stream-gen))
+(cog-set-value!
+	(Concept "source") (Predicate "key")
+	(StringValue
+		"some text\n"
+		"without a newline"
+		"after it\n"
+		"Goodbye!\n"))
 
-; The previous demo ran the input file to end-of-file; we need to
-; restart at the beginning.
-(cog-execute!
-	(SetValue (Concept "foo") (Predicate "some place")
-		(Open (Type 'TextFileStream)
-			(Sensory "file:///tmp/demo.txt"))))
+; Redefine the writer.
+(define writer
+	(WriteLink
+		(ValueOf (Concept "xterm anchor") (Predicate "output place"))
+		(ValueOf (Concept "source") (Predicate "key"))))
 
-; Run the rule, once.
-(cog-execute! rule-applier)
-
-; Repeat it again, over and over, till the end-of-file.
-(cog-execute! rule-applier)
-(cog-execute! rule-applier)
-(cog-execute! rule-applier)
-(cog-execute! rule-applier)
-(cog-execute! rule-applier)
-(cog-execute! rule-applier)
-
-; --------------------------------------------------------
-; Demo: Perform processing on the stream. In this case, parse the
-; input stream into token pairs. Use the LG "any" parser for this.
-
-(use-modules (opencog nlp) (opencog nlp lg-parse))
-
-; As above: rewind the stream to the beginning:
-(cog-execute!
-	(SetValue (Concept "foo") (Predicate "some place")
-		(Open (Type 'TextFileStream)
-			(Sensory "file:///tmp/demo.txt"))))
-
-; Parse the file contents, one line at a time. The "any" dict generates
-; random word-pairs. The (Number 1) asks for only one parse per
-; sentence.
-(cog-execute! (LgParseBonds txt-stream-gen (LgDict "any") (Number 1)))
-(cog-execute! (LgParseBonds txt-stream-gen (LgDict "any") (Number 1)))
-(cog-execute! (LgParseBonds txt-stream-gen (LgDict "any") (Number 1)))
+; Write it out.
+(cog-execute! writer)
+(cog-execute! writer)
+(cog-execute! writer)
 
 ; --------------------------------------------------------
 ; The End! That's All, Folks!
