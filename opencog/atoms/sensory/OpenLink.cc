8d2dacbeb0d1e9fed4480d3079a3234e342028d1
Linas Vepstas <linas@linas.org>
2024-12-23 20:23:58 -0600

    Bugfix the OpenLink

--- opencog/atoms/sensory/OpenLink.cc
+++ opencog/atoms/sensory/OpenLink.cc
@@ -58,10 +58,12 @@ void OpenLink::init(void)
 
 	_kind = TypeNodeCast(_outgoing[0])->get_kind();
 
-	// TODO: perhaps second argument is an executable link that
-	// returns a SensoryNode. So fix me later, someday.
+	// If there is a second argument, then it must be either a
+	// SensoryNode, or soomething executable that returns a
+	// SensoryNode.
 	if (2 <= _outgoing.size() and
-	    not _outgoing[1]->is_type(SENSORY_NODE))
+	    not _outgoing[1]->is_type(SENSORY_NODE) and
+	    not _outgoing[1]->is_executable())
 		throw SyntaxException(TRACE_INFO,
 			"Expecting the second argument to be a SensoryNode!");
 }
