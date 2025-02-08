47b36a32133e1857845d924797f961e3c04de7ef
Linas Vepstas <linas@linas.org>
2025-02-07 20:23:11 -0600

    Create a more realistic data source

--- sandbox/hamming.scm
+++ sandbox/hamming.scm
@@ -4,7 +4,23 @@
 
 (use-modules (opencog) (opencog exec))
 
-; Setup data.
+; Low-level tool to declare a relationship R(A,B)
+; R, A, B are strings
+(define (rela R A B)
+	(Edge (Predicate R) (List (Item A) (Item B))))
+
+; Setup data. Some bogus dependency parse.
+(rela "word-pair" "the" "dog")
+(rela "word-pair" "dog" "chased")
+(rela "word-pair" "the" "cat")
+(rela "word-pair" "chased" "cat")
+(rela "word-pair" "chased" "around")
+(rela "word-pair" "the" "house")
+(rela "word-pair" "around" "house")
+(rela "word-pair" "cat" "around")
+(rela "word-pair" "HEAD" "dog")
+(rela "word-pair" "HEAD" "chased")
+
 (define vec-foo (LinkValue
 	(Item "a") (Item "c") (Item "e") (Item "f") (Item "g")))
 
