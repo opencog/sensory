d0576126360d2ca415e22d634a0d00743cb3ac00
Linas Vepštas <linasvepstas@gmail.com>
2025-08-19 19:23:59 -0500

    Start porting the terminal code

---
+++ opencog/atoms/terminal/README.md
@@ -0,0 +1,41 @@
+PTY Terminal I/O
+================
+Interacting with a Unix pty terminal. So, like IRC, but a lot simpler.
+
+Examples
+--------
+Two. The `xterm-io.scm` shows basic usage, while `xterm-bridge.scm`
+shows two of them, each echoing the other. Both examples are in the
+[examples](../../../examples) directory.
+
+Design
+------
+See the [Design Notes Part C](../../../DesignNotes-C.md) for a general
+motivational discussion on why this class is needed.
+
+The actual design is kind of bizarre. What I'd originally envisioned
+was a C++ class that would open `/dev/pts/NN` and that there'd be some
+Unix terminal that I could point at this PTY and use it to send &
+receive text.
+
+There is no such tool.  One could write one, by grabbing a copy of
+the prehistoric `talk` command and hacking it. But this quickly devolves
+into hacking `inetd.conf` and yuck. Do not want to go down that path: a
+lot of work for little gain.
+
+A different design alternative is to build a telnet server, and tell
+everyone to telnet into it. This is a classic design point, I've done it
+too many times in my life so, and its not that interesting, and is not
+needed, its overkill for the intended research. Maybe some other day.
+
+The final design is brute-force stupid but ugly: open a slave xterm.
+You read that right. A skanky old xterm. It works, its super-simple
+to code up, and is more than enough to solve all the other research
+goals in this project. And so there it is. Brutish but servicable.
+
+One thing that `xterm` does that other terminals do not do is that you
+can attach it directly to `/dev/pts/NN` with the `-S` flag. Other
+(modern) terminals do not do this. Looking at you, `mate-terminal`.
+At any rate, this is "for demo purposes only", and not for production.
+
+-----------------------------------
---
+++ opencog/atoms/terminal/TerminalNode.h
@@ -0,0 +1,83 @@
+/*
+ * opencog/atoms/sensory-v0/TerminalStream.h
+ *
+ * Copyright (C) 2024 Linas Vepstas
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENCOG_TERMINAL_STREAM_H
+#define _OPENCOG_TERMINAL_STREAM_H
+
+#include <stdio.h>
+#include <opencog/atoms/sensory-v0/OutputStream.h>
+
+namespace opencog
+{
+
+/** \addtogroup grp_atomspace
+ *  @{
+ */
+
+/**
+ * TerminalStreams provide a text stream connected to an instance of an
+ * xterm. Due to the general weirdness of attaching terminals to running
+ * processes, this is what it is because the alternative is to build a
+ * telnet sserver, and I don't want to do that.
+ */
+class TerminalStream
+	: public OutputStream
+{
+protected:
+	TerminalStream(Type t, const std::string&);
+	void init(void);
+	void halt(void) const;
+	virtual void update() const;
+
+	Handle _description;
+	void do_describe(void);
+
+	mutable FILE* _fh;
+	mutable pid_t _xterm_pid;
+	virtual void do_write(const std::string&);
+
+public:
+	TerminalStream(void);
+	TerminalStream(const ValueSeq&);
+	virtual ~TerminalStream();
+
+	virtual ValuePtr describe(AtomSpace*, bool);
+	virtual ValuePtr write_out(AtomSpace*, bool, const Handle&);
+};
+
+typedef std::shared_ptr<TerminalStream> TerminalStreamPtr;
+static inline TerminalStreamPtr TerminalStreamCast(ValuePtr& a)
+	{ return std::dynamic_pointer_cast<TerminalStream>(a); }
+
+template<typename ... Type>
+static inline std::shared_ptr<TerminalStream> createTerminalStream(Type&&... args) {
+   return std::make_shared<TerminalStream>(std::forward<Type>(args)...);
+}
+
+/** @}*/
+} // namespace opencog
+
+extern "C" {
+void opencog_sensory_v0_terminal_init(void);
+};
+
+#endif // _OPENCOG_TERMINAL_STREAM_H
