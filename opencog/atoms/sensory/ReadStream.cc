ec9b25c10f38066bee0aca58fdff2f8da79f11d4
Linas Vepštas <linasvepstas@gmail.com>
2025-08-21 12:21:32 -0500

    Add a stream reader stub

---
+++ opencog/atoms/sensory/ReadStream.cc
@@ -0,0 +1,61 @@
+/*
+ * opencog/atoms/sensory/ReadStream.cc
+ *
+ * Copyright (C) 2025 Linas Vepstas
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <opencog/util/exceptions.h>
+#include <opencog/util/oc_assert.h>
+#include <opencog/atoms/base/Node.h>
+#include <opencog/atoms/value/StringValue.h>
+#include <opencog/atoms/value/ValueFactory.h>
+
+#include <opencog/sensory/types/atom_types.h>
+#include "ReadStream.h"
+
+using namespace opencog;
+
+ReadStream::ReadStream(const Handle& senso)
+	: LinkStreamValue(READ_STREAM)
+{
+	if (SENSORY_NODE != senso->get_type())
+		throw RuntimeException(TRACE_INFO,
+			"Expecting SensoryNode, got %s\n", senso->to_string().c_str());
+}
+
+ReadStream::~ReadStream()
+{
+}
+
+// ==============================================================
+
+// This will read one item at a time from the object, and return
+// that item.
+// So, still, a line-oriented, buffered interface. For now.
+void ReadStream::update() const
+{
+
+}
+
+// ==============================================================
+
+// Adds factory when library is loaded.
+DEFINE_VALUE_FACTORY(READ_STREAM, createReadStream, Handle)
+
+// ====================================================================
---
+++ opencog/atoms/sensory/ReadStream.h
@@ -0,0 +1,61 @@
+/*
+ * opencog/atoms/sensory/ReadStream.h
+ *
+ * Copyright (C) 2024 Linas Vepstas
+ * Copyright (C) 2025 BrainyBlaze Dynamics Inc.
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENCOG_READ_STREAM_H
+#define _OPENCOG_READ_STREAM_H
+
+#include <stdio.h>
+#include <opencog/atoms/value/LinkStreamValue.h>
+
+namespace opencog
+{
+
+/** \addtogroup grp_atomspace
+ *  @{
+ */
+
+/**
+ * ReadStream wraps ObjectNodes that provide one-at-a-time *-read-*
+ * methods, and provides a stream of values obtained by reading,
+ * on demand.
+ *
+ * This is experimental.
+ */
+class ReadStream
+	: public LinkStreamValue
+{
+protected:
+	virtual void update() const;
+
+public:
+	ReadStream(const Handle&);
+	virtual ~ReadStream();
+};
+
+VALUE_PTR_DECL(ReadStream)
+CREATE_VALUE_DECL(ReadStream)
+
+/** @}*/
+} // namespace opencog
+
+#endif // _OPENCOG_READ_STREAM_H
