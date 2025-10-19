93f616b17d5a485a6c12eeaba08cb950df293781
Linas Vepstas <linasvepstas@gmail.com>
2025-10-19 04:01:52 +0000

    Start refactoring common inotify code.
    Both FileSys and TextFile will use this.

---
+++ opencog/atoms/filedir/FileWatcher.cc
@@ -0,0 +1,146 @@
+/*
+ * opencog/atoms/filedir/FileWatcher.cc
+ *
+ * Copyright (C) 2025 BrainyBlaze Dynamics, Inc.
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <errno.h>
+#include <string.h>
+#include <sys/inotify.h>
+#include <unistd.h>
+#include <limits.h>
+
+#include <opencog/util/exceptions.h>
+
+#include "FileWatcher.h"
+
+using namespace opencog;
+
+FileWatcher::FileWatcher() :
+	_inotify_fd(-1),
+	_watch_fd(-1),
+	_watch_path(),
+	_event_mask(0)
+{
+}
+
+FileWatcher::~FileWatcher()
+{
+	cleanup_watch();
+	cleanup_inotify();
+}
+
+void FileWatcher::cleanup_watch()
+{
+	if (_watch_fd >= 0 && _inotify_fd >= 0)
+	{
+		inotify_rm_watch(_inotify_fd, _watch_fd);
+		_watch_fd = -1;
+	}
+	_watch_path.clear();
+	_event_mask = 0;
+}
+
+void FileWatcher::cleanup_inotify()
+{
+	if (_inotify_fd >= 0)
+	{
+		::close(_inotify_fd);
+		_inotify_fd = -1;
+	}
+}
+
+void FileWatcher::add_watch(const std::string& path, uint32_t mask)
+{
+	// Remove any existing watch first
+	if (_watch_fd >= 0)
+		cleanup_watch();
+
+	// Initialize inotify if not already done
+	if (_inotify_fd < 0)
+	{
+		_inotify_fd = inotify_init();
+		if (_inotify_fd < 0)
+		{
+			int norr = errno;
+			throw RuntimeException(TRACE_INFO,
+				"Failed to initialize inotify: %s\n", strerror(norr));
+		}
+	}
+
+	// Add watch on the path
+	_watch_fd = inotify_add_watch(_inotify_fd, path.c_str(), mask);
+	if (_watch_fd < 0)
+	{
+		int norr = errno;
+		cleanup_inotify();
+		throw RuntimeException(TRACE_INFO,
+			"Failed to add inotify watch on \"%s\": %s\n",
+			path.c_str(), strerror(norr));
+	}
+
+	_watch_path = path;
+	_event_mask = mask;
+}
+
+void FileWatcher::remove_watch()
+{
+	cleanup_watch();
+	cleanup_inotify();
+}
+
+std::pair<uint32_t, std::string> FileWatcher::wait_event()
+{
+	if (_inotify_fd < 0 || _watch_fd < 0)
+	{
+		throw RuntimeException(TRACE_INFO,
+			"FileWatcher::wait_event() called without active watch\n");
+	}
+
+	// Buffer for inotify events
+	// Must be large enough for at least one event plus the largest possible filename
+	char event_buf[sizeof(struct inotify_event) + NAME_MAX + 1];
+
+	while (true)
+	{
+		ssize_t len = ::read(_inotify_fd, event_buf, sizeof(event_buf));
+
+		if (len < 0)
+		{
+			// Interrupted by signal - retry
+			if (errno == EINTR)
+				continue;
+
+			// Real error
+			int norr = errno;
+			throw RuntimeException(TRACE_INFO,
+				"inotify read failed: %s\n", strerror(norr));
+		}
+
+		// Parse the event
+		struct inotify_event* event = (struct inotify_event*)event_buf;
+
+		// Extract filename if present (for directory watches)
+		std::string filename;
+		if (event->len > 0)
+			filename = std::string(event->name);
+
+		return std::make_pair(event->mask, filename);
+	}
+}
---
+++ opencog/atoms/filedir/FileWatcher.h
@@ -0,0 +1,115 @@
+/*
+ * opencog/atoms/filedir/FileWatcher.h
+ *
+ * Copyright (C) 2025 BrainyBlaze Dynamics, Inc.
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENCOG_FILE_WATCHER_H
+#define _OPENCOG_FILE_WATCHER_H
+
+#include <string>
+#include <utility>
+
+namespace opencog
+{
+/** \addtogroup grp_atomspace
+ *  @{
+ */
+
+/**
+ * FileWatcher - Abstraction for inotify-based file and directory watching
+ *
+ * This class encapsulates the common inotify functionality needed for
+ * monitoring files and directories for changes. It manages the inotify
+ * file descriptors, watch setup/teardown, and event reading.
+ *
+ * Usage pattern:
+ *   FileWatcher watcher;
+ *   watcher.add_watch("/path/to/file", IN_MODIFY | IN_CLOSE_WRITE);
+ *
+ *   while (true) {
+ *       auto [event_mask, filename] = watcher.wait_event();
+ *       // Process event
+ *   }
+ *
+ *   watcher.remove_watch();
+ */
+class FileWatcher
+{
+private:
+	int _inotify_fd;
+	int _watch_fd;
+	std::string _watch_path;
+	uint32_t _event_mask;
+
+	void cleanup_watch();
+	void cleanup_inotify();
+
+public:
+	FileWatcher();
+	~FileWatcher();
+
+	// Prevent copying
+	FileWatcher(const FileWatcher&) = delete;
+	FileWatcher& operator=(const FileWatcher&) = delete;
+
+	/**
+	 * Add a watch on the specified path with the given event mask.
+	 *
+	 * @param path The file or directory path to watch
+	 * @param mask The inotify event mask (e.g., IN_MODIFY | IN_CLOSE_WRITE)
+	 * @throws RuntimeException if inotify initialization or watch setup fails
+	 */
+	void add_watch(const std::string& path, uint32_t mask);
+
+	/**
+	 * Remove the current watch and cleanup.
+	 */
+	void remove_watch();
+
+	/**
+	 * Wait for the next inotify event (blocking).
+	 *
+	 * @return A pair of (event_mask, filename) where:
+	 *         - event_mask: The inotify event that occurred
+	 *         - filename: The name of the file (for directory watches)
+	 *                    or empty string (for file watches)
+	 * @throws RuntimeException if inotify read fails (non-EINTR error)
+	 */
+	std::pair<uint32_t, std::string> wait_event();
+
+	/**
+	 * Check if currently watching a path.
+	 *
+	 * @return true if a watch is active, false otherwise
+	 */
+	bool is_watching() const { return _watch_fd >= 0; }
+
+	/**
+	 * Get the currently watched path.
+	 *
+	 * @return The path being watched, or empty string if not watching
+	 */
+	const std::string& watched_path() const { return _watch_path; }
+};
+
+/** @}*/
+} // namespace opencog
+
+#endif // _OPENCOG_FILE_WATCHER_H
