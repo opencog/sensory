086fde608fb9f21bd6594d6dbe2586af88c8b926
Linas Vepstas <linas@linas.org>
2026-01-03 21:36:02 -0600

    More monkeying about

--- sandbox/count.scm
+++ sandbox/count.scm
@@ -82,5 +82,48 @@
 			(IncrementValue (Variable "$typ") (Predicate "cnt") (Number 0 0 1)))
 		(Name "get-types")))
 
+; Must actually run this one...
+; Can we have an Atom that just runs where created?
 (cog-execute! (Name "count-types"))
+
+(Pipe
+	(Name "unique-types")
+	(CollectionOf (TypeNode 'UnisetValue)
+		(Name "get-types")))
+
+; (cog-execute!  (Name "unique-types"))
+
+(DefineLink
+	(DefinedPredicate "count-order")
+	(Lambda
+		(VariableList (Variable "left") (Variable "right"))
+		(Not
+			(GreaterThan
+				(ElementOf (Number 2)
+					(ValueOf (Variable "left") (Predicate "cnt")))
+				(ElementOf (Number 2)
+					(ValueOf (Variable "right") (Predicate "cnt")))))))
+
+(define x
+	(SortedValue
+		(DefinedPredicate "count-order")
+		(Name "unique-types")))
+
+
+; Debug print
+(cog-execute!
+	(Filter
+		(Rule
+			(TypedVariable (Variable "$typ") (Type 'Type)) ; vardecl
+			(Variable "$typ") ; body - accept everything
+			(LinkSignature (Type 'LinkValue)
+				(Variable "$typ")
+				(ValueOf (Variable "$typ") (Predicate "cnt"))))
+		(Name "unique-types")))
+
+
+(cog-execute!
+	(CollectionOf 
+(TypeNode 'SortedValue)
+		(Name "unique-types")))
 
